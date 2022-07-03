05be42bb980534fd9c36370250f034076b5b083c
carlo <carlos.cubillos@urosario.edu.co>
2022-07-03 12:47:53 -0500

    cambios

--- pages/01_Exploratory_Data_Analysis_.py
+++ pages/01_Exploratory_Data_Analysis_.py
@@ -41,21 +41,21 @@ Expo_4= st.container()
 #url_5 = 'https://eficaciadata.s3.amazonaws.com/ago_pdv_pro.csv' # Data of products out stock for store and type of product
 #url_6 = 'https://eficaciadata.s3.amazonaws.com/ago.csv' # DATA DE AGOTADOS
 
-url_1 = 'Atlasteroids\csv\geodata.csv' # Data of geographic points of the stores
-url_2 = 'Atlasteroids\csv\pro_pre_pdv.csv' # Data of products-Prices-stores
-url_3 = 'Atlasteroids\csv\pre.csv'# Data of prices
-url_4 = 'Atlasteroids\csv\pro.csv' # DATA DE PRODUCTOS
-url_5 = 'Atlasteroids\csv\1ago_pdv_pro.csv' # Data of products out stock for store and type of product
-url_6 = 'Atlasteroids\csv\1ago.csv' # DATA DE AGOTADOS
+url_1 = 'Atlasteroids/csv/geodata.csv' # Data of geographic points of the stores
+url_2 = 'Atlasteroids/csv/pro_pre_pdv.csv' # Data of products-Prices-stores
+url_3 = 'Atlasteroids/csv/pre.csv'# Data of prices
+url_4 = 'Atlasteroids/csv/pro.csv' # DATA DE PRODUCTOS
+url_5 = 'Atlasteroids/csv/ago_pdv_pro.csv' # Data of products out stock for store and type of product
+url_6 = 'Atlasteroids/csv/ago.csv' # DATA DE AGOTADOS
 #uses this instruccion it the data change @st.cache(persist=True)( If you have a different use case where the data does not change so very often, you can simply use this)
 
 #loading data
 gea = pd.read_csv(url_1, sep=';')
-data_2 = pd.read_csv(url_2)
-data_3 = pd.read_csv(url_3)
-data_4 = pd.read_csv(url_4)
-data_5 = pd.read_csv(url_5)
-#data_6 = pd.read_csv(url_6)
+data_2 = pd.read_csv(url_2, sep=',')
+data_3 = pd.read_csv(url_3, sep=',')
+data_4 = pd.read_csv(url_4, sep=',')
+data_5 = pd.read_csv(url_5, sep=',')
+data_6 = pd.read_csv(url_6, sep=',')
 
 
 with Expo_1:
